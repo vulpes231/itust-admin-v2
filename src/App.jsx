94f8502228a7e0eb43fcb9a75d4fb58037410054
Vulpes231 <whykayshot@gmail.com>
2026-01-08 09:08:22 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import Scss
+import "./assets/scss/themes.scss";
+
+//imoprt Route
+import Route from "./Routes";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Route />
+    </React.Fragment>
+  );
+}
+
+export default App;
